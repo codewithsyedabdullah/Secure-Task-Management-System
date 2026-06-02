4d7305f98b5e0c958081608b45246dafb65fd057
Syed Abdullah Yaqoob <abdullah2684511@gmail.com>
2026-06-02 17:44:50 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -35,8 +35,8 @@ app.use(session({
   saveUninitialized: false,
   cookie: {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+    secure: true,
+    sameSite: 'none',
     maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
   },
 }));
